465d9c70597a6f5a58753bf96e6aa65fe77bb659
sawcao <q649691009@hotmail.com>
2023-12-17 00:27:25 +0800

    modified:   jobs/ak_test.py

--- jobs/ak_test.py
+++ jobs/ak_test.py
@@ -1,4 +1,4 @@
 import akshare as ak
 
-stock_zh_a_minute_df = ak.stock_zh_a_minute(symbol='sh600172', period='1', adjust="qfq")
+stock_zh_a_minute_df = ak.stock_zh_a_minute(symbol='sh603231', period='1', adjust="qfq")
 print(stock_zh_a_minute_df)
