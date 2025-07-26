0475705942f0aa5eda4ab1703b30da5c9448f76d
sawcao <sawcao@github.com>
2025-07-26 19:54:34 +0800

    update

--- web/new_web/app.py
+++ web/new_web/app.py
@@ -235,7 +235,7 @@ def get_stock_from_db_with_search(search_string, gain_threshold, start_date, end
         database='stock_data',
         cursorclass=pymysql.cursors.DictCursor
     )
-    #pro = ts.pro_api('0a42c03559605acecb58cca7218b5f6736f1ea878e20a090b2077bdf')
+    pro = ts.pro_api('0a42c03559605acecb58cca7218b5f6736f1ea878e20a090b2077bdf')
     try:
         with connection.cursor() as cursor:
             # Get parameters from request
