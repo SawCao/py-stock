20afd631cbf16acf8f2337b71808465245567fc6
sawcao <sawcao@github.com>
2025-07-26 19:15:20 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch dev # Your branch is up to date with 'origin/dev'. # # Changes to be committed: #	modified:   build_and_push_ui.sh #	new file:   deploy_ui.sh #

--- build_and_push_ui.sh
+++ build_and_push_ui.sh
@@ -13,6 +13,9 @@ DOCKERFILE_PATH="web/umi-stock/Dockerfile"
 CONTEXT_PATH="web/umi-stock/"
 
 # --- 脚本开始 ---
+export http_proxy="http://127.0.0.1:10809"
+export https_proxy="http://127.0.0.1:10809"
+
 set -e # 如果任何命令失败，则立即退出
 
 echo "=================================================="
